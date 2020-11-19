30bb1ebe0514cff0ba4f237f7ec3e24428bcbcec
Adinarayanreloaded <adinarayanofficial@gmail.com>
2020-11-19 17:40:59 +0530

    Update binarysearch.c
    Updated Binary Search program here

--- Searching/binarysearch.c
+++ Searching/binarysearch.c
@@ -1,37 +1,26 @@
 #include <stdio.h>
-int main()
-{
-  int c, first, last, middle, n, search, array[100];
-
-  printf("Enter number of elements\n");
-  scanf("%d", &n);
-
-  printf("Enter %d integers\n", n);
-
-  for (c = 0; c < n; c++)
-    scanf("%d", &array[c]);
-
-  printf("Enter value to find\n");
-  scanf("%d", &search);
-
-  first = 0;
-  last = n - 1;
-  middle = (first+last)/2;
-
-  while (first <= last) {
-    if (array[middle] < search)
-      first = middle + 1;
-    else if (array[middle] == search) {
-      printf("%d found at location %d.\n", search, middle+1);
-      break;
-    }
-    else
-      last = middle - 1;
-
-    middle = (first + last)/2;
-  }
-  if (first > last)
-    printf("Not found! %d isn't present in the list.\n", search);
-
-  return 0;
+int iterativeBinarySearch(int array[], int start_index, int end_index, int element){
+   while (start_index <= end_index){
+      int middle = start_index + (end_index- start_index )/2;
+      if (array[middle] == element)
+         return middle;
+      if (array[middle] < element)
+         start_index = middle + 1;
+      else
+         end_index = middle - 1;
+   }
+   return -1;
+}
+int main(void){
+   int array[] = {1, 4, 7, 9, 16, 56, 70};
+   int n = 7;
+   int element = 16;
+   int found_index = iterativeBinarySearch(array, 0, n-1, element);
+   if(found_index == -1 ) {
+      printf("Element not found in the array ");
+   }
+   else {
+      printf("Element found at index : %d",found_index);
+   }
+   return 0;
 }
